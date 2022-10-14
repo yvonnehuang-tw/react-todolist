3b1969ab7554a8827122a6a030d6dec6206bfefa
Yvonne Huang <yvonne.st.huang@dlinkcorp.com>
2022-10-14 15:52:33 +0800

    Add Header, Main, Footer

---
+++ src/components/Header.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+const Header = () => {
+  const NAVLIST = [
+    {
+      url: "#",
+      icon: "icon-list-ul",
+    },
+    // {
+    //   url: "#",
+    //   icon: "icon-user",
+    // },
+    // {
+    //   url: "#",
+    //   icon: "icon-heart",
+    // },
+    // {
+    //   url: "#",
+    //   icon: "icon-shopping-cart",
+    // },
+    {
+      url: "",
+      icon: "icon-adjust",
+    },
+  ];
+
+  const [defaultMode, setDefaultMode] = useState("light-background");
+
+  const clickHandler = (e) => {
+    e.preventDefault();
+    if (e.target.className === "icon-adjust") {
+      // Change dark/light theme
+      if (defaultMode === "light-background") {
+        setDefaultMode("dark-background");
+      } else {
+        setDefaultMode("light-background");
+      }
+    }
+  };
+
+  return (
+    <header className={`header ${defaultMode}`}>
+      <h4 className="logo">Todolist</h4>
+      <nav>
+        <ul className="navigation">
+          {NAVLIST.map((list) => {
+            return (
+              <li key={list.icon}>
+                <a href={list.url} onClick={clickHandler}>
+                  <i className={list.icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
