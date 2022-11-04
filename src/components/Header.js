cb4df44f60d1d1517c282a3d38edd838a482e046
Yvonne Huang <yvonne.st.huang@dlinkcorp.com>
2022-11-04 10:47:23 +0800

    Header use rettier settings.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,48 @@
-import { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
-let title = "Demo";
-let theme = localStorage.getItem("theme") || "light-background";
+let title = 'Demo';
+let theme = localStorage.getItem('theme') || 'light-background';
 const Header = () => {
   const NAVLIST = [
     {
-      url: "/todolist",
-      icon: "icon-list-ul",
-      name: "TodoList",
+      url: '/todolist',
+      icon: 'icon-list-ul',
+      name: 'TodoList',
     },
     {
-      url: "/table-use-state",
-      icon: "icon-table",
-      name: "Table use state",
+      url: '/table-use-state',
+      icon: 'icon-table',
+      name: 'Table use state',
     },
     {
-      url: "/table-use-reducer",
-      icon: "icon-th-list",
-      name: "Table use reducer",
+      url: '/table-use-reducer',
+      icon: 'icon-th-list',
+      name: 'Table use reducer',
     },
   ];
 
   const location = useLocation();
   if (location && location.pathname) {
-    if (location.pathname.slice(1) === "todolist") {
-      title = "todolist";
-    } else if (location.pathname.slice(1) === "table") {
-      title = "table";
+    if (location.pathname.slice(1) === 'todolist') {
+      title = 'todolist';
+    } else if (location.pathname.slice(1) === 'table') {
+      title = 'table';
     }
   }
 
   const [defaultMode, setDefaultMode] = useState(theme);
   const [headerName, setHeaderName] = useState(title);
 
-  const handleClickChangeTheme = (e) => {
+  const handleClickChangeTheme = e => {
     e.preventDefault();
-    if (e.target.className === "icon-adjust") {
-      if (defaultMode === "light-background") {
-        setDefaultMode("dark-background");
-        localStorage.setItem("theme", "dark-background");
+    if (e.target.className === 'icon-adjust') {
+      if (defaultMode === 'light-background') {
+        setDefaultMode('dark-background');
+        localStorage.setItem('theme', 'dark-background');
       } else {
-        setDefaultMode("light-background");
-        localStorage.setItem("theme", "light-background");
+        setDefaultMode('light-background');
+        localStorage.setItem('theme', 'light-background');
       }
     }
   };
@@ -50,13 +50,13 @@ const Header = () => {
   return (
     <header className={`header ${defaultMode}`}>
       <h4 className="logo">
-        <Link to="/" onClick={() => setHeaderName("Demo")}>
+        <Link to="/" onClick={() => setHeaderName('Demo')}>
           {headerName}
         </Link>
       </h4>
       <nav>
         <ul className="navigation">
-          {NAVLIST.map((list) => {
+          {NAVLIST.map(list => {
             return (
               <li key={list.icon}>
                 <Link to={list.url} onClick={() => setHeaderName(list.name)}>
@@ -66,7 +66,11 @@ const Header = () => {
             );
           })}
           <li>
-            <a href="#/" style={{ cursor: "pointer" }} onClick={handleClickChangeTheme}>
+            <a
+              href="#/"
+              style={{ cursor: 'pointer' }}
+              onClick={handleClickChangeTheme}
+            >
               <i className="icon-adjust"></i>
             </a>
           </li>
